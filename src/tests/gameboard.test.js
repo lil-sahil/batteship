77f56d2020abcce6a5d5eb47b7c4e5a7cd9543fa
lil-sahil <sahilnagpal93@yahoo.ca>
2022-03-12 14:25:03 -0500

    Modified test to return false if illegal placement is specified to place a ship.

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -63,19 +63,7 @@ test("Place ship vertically out of board", () => {
   let game = gameboardFactory();
   game.createGameboard();
   let ship = shipFactory(3);
-  let answer = {
-    row_0: ["", "", "", "", "", "", "", "", "", ""],
-    row_1: ["", "", "", "", "", "", "", "", "", ""],
-    row_2: ["", "", "", "", "", "", "", "", "", ""],
-    row_3: ["", "", "", "", "", "", "", "", "", ""],
-    row_4: ["", "", "", "", "", "", "", "", "", ""],
-    row_5: ["", "", "", "", "", "", "", "", "", ""],
-    row_6: ["", "", "", "", "", "", "", "", "", ""],
-    row_7: ["", "", "", "", "", "", "", "", "", ""],
-    row_8: ["", "", "", "", "", "", "", "", "", ""],
-    row_9: ["", "", "", "", "", "", "", "", "", ""],
-  };
-  expect(game.placeShip(9, 0, ship, "vertical")).toEqual(answer);
+  expect(game.placeShip(9, 0, ship, "vertical")).toBe(false);
 });
 
 test("Place ship vertically in in-accesible position", () => {
@@ -85,38 +73,15 @@ test("Place ship vertically in in-accesible position", () => {
   let ship2 = shipFactory(3);
   game.placeShip(0, 0, ship1, "vertical");
 
-  let answer = {
-    row_0: ["-", "", "", "", "", "", "", "", "", ""],
-    row_1: ["-", "", "", "", "", "", "", "", "", ""],
-    row_2: ["-", "", "", "", "", "", "", "", "", ""],
-    row_3: ["", "", "", "", "", "", "", "", "", ""],
-    row_4: ["", "", "", "", "", "", "", "", "", ""],
-    row_5: ["", "", "", "", "", "", "", "", "", ""],
-    row_6: ["", "", "", "", "", "", "", "", "", ""],
-    row_7: ["", "", "", "", "", "", "", "", "", ""],
-    row_8: ["", "", "", "", "", "", "", "", "", ""],
-    row_9: ["", "", "", "", "", "", "", "", "", ""],
-  };
-  expect(game.placeShip(1, 0, ship2, "vertical")).toEqual(answer);
+  expect(game.placeShip(1, 0, ship2, "vertical")).toBe(false);
 });
 
 test("Place ship horizontaly out of board", () => {
   let game = gameboardFactory();
   game.createGameboard();
   let ship = shipFactory(3);
-  let answer = {
-    row_0: ["", "", "", "", "", "", "", "", "", ""],
-    row_1: ["", "", "", "", "", "", "", "", "", ""],
-    row_2: ["", "", "", "", "", "", "", "", "", ""],
-    row_3: ["", "", "", "", "", "", "", "", "", ""],
-    row_4: ["", "", "", "", "", "", "", "", "", ""],
-    row_5: ["", "", "", "", "", "", "", "", "", ""],
-    row_6: ["", "", "", "", "", "", "", "", "", ""],
-    row_7: ["", "", "", "", "", "", "", "", "", ""],
-    row_8: ["", "", "", "", "", "", "", "", "", ""],
-    row_9: ["", "", "", "", "", "", "", "", "", ""],
-  };
-  expect(game.placeShip(9, 9, ship, "horizontal")).toEqual(answer);
+
+  expect(game.placeShip(9, 9, ship, "horizontal")).toBe(false);
 });
 
 test("Place ship horizontaly in in-accesible position", () => {
@@ -127,19 +92,7 @@ test("Place ship horizontaly in in-accesible position", () => {
   let ship2 = shipFactory(3);
   game.placeShip(0, 0, ship1, "horizontal");
 
-  let answer = {
-    row_0: ["-", "-", "-", "", "", "", "", "", "", ""],
-    row_1: ["", "", "", "", "", "", "", "", "", ""],
-    row_2: ["", "", "", "", "", "", "", "", "", ""],
-    row_3: ["", "", "", "", "", "", "", "", "", ""],
-    row_4: ["", "", "", "", "", "", "", "", "", ""],
-    row_5: ["", "", "", "", "", "", "", "", "", ""],
-    row_6: ["", "", "", "", "", "", "", "", "", ""],
-    row_7: ["", "", "", "", "", "", "", "", "", ""],
-    row_8: ["", "", "", "", "", "", "", "", "", ""],
-    row_9: ["", "", "", "", "", "", "", "", "", ""],
-  };
-  expect(game.placeShip(0, 1, ship2, "horizontal")).toEqual(answer);
+  expect(game.placeShip(0, 1, ship2, "horizontal")).toBe(false);
 });
 
 test("Place multiple ships", () => {
